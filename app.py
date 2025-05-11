cf3319a4c051e74670ab44ff390af5c04a196181
Gre1971 <gre0549404799@gmail.com>
2025-05-11 16:52:17 +0300

    app.py

--- app.py
+++ app.py
@@ -19,3 +19,8 @@ def convert():
         "message": "File received and saved",
         "filename": filename
     })
+
+# נדרש על ידי Render כדי להאזין לפורט מתאים
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
